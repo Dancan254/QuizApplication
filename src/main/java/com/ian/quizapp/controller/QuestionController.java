621d3f678fcc75b05bedbbf86c0d9c84143bf0b9
Ian Dancan Mongare <dancanian25@gmail.com>
2024-04-29 10:53:02 +0300

    implemented swagger documentation

--- src/main/java/com/ian/quizapp/controller/QuestionController.java
+++ src/main/java/com/ian/quizapp/controller/QuestionController.java
@@ -30,23 +30,58 @@ public class QuestionController {
                                             array = @ArraySchema(schema = @Schema(implementation = QuestionDto.class)))
                             }
                     )
-
             }
     )
-    @GetMapping("/allQuestions")
+    @GetMapping("/questions/all")
     public ResponseEntity<List<QuestionDto>> getAllQuestions(){
         return new ResponseEntity<>(questionService.getAllQuestions(), HttpStatus.OK);
     }
 
+
+    @Operation(summary = "get questions by category")
+    @ApiResponses(
+            value = {
+                    @ApiResponse(responseCode = "302", description = "questions fetched successfully",
+                            content = {
+                                    @Content(mediaType = "application/json",
+                                            array = @ArraySchema(schema = @Schema(implementation = QuestionDto.class)))
+                            }
+                    )
+            }
+    )
     @GetMapping("/category/{category}")
     public ResponseEntity<List<QuestionDto>> getQuestionsByCategory(@PathVariable String category){
         return new ResponseEntity<>(questionService.getQuestionsByCategory(category), HttpStatus.FOUND);
     }
+    //document the api
+    @Operation(summary = "get questions by difficulty")
+    @ApiResponses(
+            value = {
+                    @ApiResponse(responseCode = "200", description = "questions fetched successfully",
+                            content = {
+                                    @Content(mediaType = "application/json",
+                                            array = @ArraySchema(schema = @Schema(implementation = QuestionDto.class)))
+                            }
+                    )
+            }
+    )
     @GetMapping("/difficulty/{difficulty}")
     public ResponseEntity<List<QuestionDto>> getQuestionsByDifficulty(@PathVariable String difficulty){
         return new ResponseEntity<>(questionService.getQuestionsByDifficulty(difficulty), HttpStatus.OK);
     }
 
+    //document the api
+    @Operation(summary = "add a question")
+    @ApiResponses(
+            value = {
+                    @ApiResponse(responseCode = "201", description = "question added successfully",
+                            content = {
+                                    @Content(mediaType = "application/json",
+                                            schema = @Schema(implementation = Question.class))
+                            }
+                    )
+            }
+    )
     @PostMapping("/add")
     public ResponseEntity<String> addQuestion(@RequestBody Question question){
         return new ResponseEntity<>(questionService.addQuestion(question), HttpStatus.CREATED);
--- src/main/java/com/ian/quizapp/controller/QuizController.java
+++ src/main/java/com/ian/quizapp/controller/QuizController.java
@@ -1,6 +1,9 @@
 package com.ian.quizapp.controller;
 
 import com.ian.quizapp.services.QuizService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -12,6 +15,13 @@ public class QuizController {
     @Autowired
     QuizService quizService;
 
+    //document the api
+    @Operation(summary = "create a quiz")
+    @ApiResponses(
+            value = {
+                    @ApiResponse(responseCode = "201", description = "quiz created successfully")
+            }
+    )
     @PostMapping("/create")
     public ResponseEntity<String> createQuiz(@RequestParam String category, @RequestParam int numq, @RequestParam String title){
 
